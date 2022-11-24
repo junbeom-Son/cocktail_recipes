0ff6369d801175bded7e22c0a206f4213bf694d4
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-24 20:51:47 +0900

    refactor : use @RestController instead of @Controller

--- src/main/java/com/example/cocktail/controller/cocktailController.java
+++ src/main/java/com/example/cocktail/controller/cocktailController.java
@@ -10,11 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 public class cocktailController {
 
     @Autowired
