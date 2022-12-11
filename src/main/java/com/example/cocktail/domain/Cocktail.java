ba1313f767bdaa3c44a55e73467611924cef7882
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-12-11 21:23:21 +0900

    fix : use @UpdateTimeStamp instead of @LastModified and remove useless methods

--- src/main/java/com/example/cocktail/domain/Cocktail.java
+++ src/main/java/com/example/cocktail/domain/Cocktail.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -26,7 +26,7 @@ public class Cocktail {
     String imageAttribution;
     @CreationTimestamp
     private LocalDateTime createdAt;
-    @LastModifiedDate
+    @UpdateTimestamp
     private LocalDateTime lastModified;
     @OneToMany(
             mappedBy = "cocktail",
@@ -76,39 +76,5 @@ public class Cocktail {
 
     public void addIngredient(CocktailIngredient cocktailIngredient) {
         cocktailIngredients.add(cocktailIngredient);
-    }
-
-    /**
-     * ingredientNo 가 일치하는 재료를 반환한다.
-     * @param ingredientNo
-     * @return 있으면 존재하는 cocktailIngredient, 없다면 null return
-     * null 을 리턴할 수 있기 때문에 사용하는 쪽에서 주의해야한다.
-     */
-    public CocktailIngredient getIngredientByNo(Long ingredientNo) {
-        for (CocktailIngredient cocktailIngredient : cocktailIngredients) {
-            if (cocktailIngredient.getId().getIngredientNo() == ingredientNo) {
-                return cocktailIngredient;
-            }
-        }
-        return null;
-    }
-
-    public CocktailIngredient getIngredientById(Long ingredientId) {
-        for (CocktailIngredient cocktailIngredient : cocktailIngredients) {
-            if (cocktailIngredient.getId().getIngredientId() == ingredientId) {
-                return cocktailIngredient;
-            }
-        }
-        return null;
-    }
-
-    public void removeIngredient(CocktailIngredient duplicatedIngredient) {
-        Long ingredientNo = duplicatedIngredient.getId().getIngredientNo();
-        cocktailIngredients.remove(duplicatedIngredient);
-        for (CocktailIngredient cocktailIngredient : cocktailIngredients) {
-            if (cocktailIngredient.getId().getIngredientNo() > ingredientNo) {
-                cocktailIngredient.getId().setIngredientNo(cocktailIngredient.getId().getIngredientNo() - 1);
-            }
-        }
     }
 }
