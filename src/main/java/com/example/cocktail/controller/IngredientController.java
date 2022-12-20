f2f27dd80f0358ab6fe82a325641ef0548e374e6
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-12-20 21:38:21 +0900

    feat : find ingredient by id

--- src/main/java/com/example/cocktail/controller/IngredientController.java
+++ src/main/java/com/example/cocktail/controller/IngredientController.java
@@ -2,6 +2,7 @@ package com.example.cocktail.controller;
 
 import com.example.cocktail.domain.Ingredient;
 import com.example.cocktail.service.IngredientService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,17 @@ public class IngredientController {
 
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
+    }
+
+    /**
+     * find ingredient by id
+     * @param ingredientID
+     * @return ingredient if it exists.
+     * or return null
+     */
+    @GetMapping("/{ingredientID}")
+    public Ingredient findIngredientById(@PathVariable Long ingredientID) {
+        return ingredientService.findIngredientById(ingredientID);
     }
 
     @PostMapping("/{ingredientID}")
